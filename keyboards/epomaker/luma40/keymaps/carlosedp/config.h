2c8fc2fdadfeb7f18c0240501f7c4e4112d5b45c
Carlos de Paula <me@carlosedp.com>
2026-05-03 12:53:00 -0300

    Add dynamic keymap layer support and modify keymap for add F keys on Luma40

--- keyboards/epomaker/luma40/keymaps/carlosedp/config.h
+++ keyboards/epomaker/luma40/keymaps/carlosedp/config.h
@@ -17,6 +17,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
 // Custom configuration
 // LUMA40 mode indicator LED indices - using keys that should be visible
 #undef LED_BLE_1_INDEX
--- keyboards/epomaker/luma40/keymaps/carlosedp/keymap.c
+++ keyboards/epomaker/luma40/keymaps/carlosedp/keymap.c
@@ -26,25 +26,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,      KC_E,     KC_R,       KC_T,      KC_Y,     KC_U,    KC_I,    KC_O,     KC_P,     KC_BSPC,
         QK_GESC, KC_A,    KC_S,      KC_D,     KC_F,       KC_G,      KC_H,     KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_ENT,
         KC_LSFT, KC_Z,    KC_X,      KC_C,     KC_V,       KC_B,      KC_N,     KC_M,    KC_COMM, KC_DOT,   KC_UP ,   KC_QUOT,
-        MO(2),   KC_LCTL, KC_GRV,    KC_LGUI,  KC_LALT,               KC_SPC,   KC_RALT, KC_SLSH, KC_LEFT,  KC_DOWN,  KC_RGHT
+        MO(2),   KC_LCTL, KC_GRV,    KC_LGUI,  KC_LALT,               KC_SPC,   MO(4),   KC_SLSH, KC_LEFT,  KC_DOWN,  KC_RGHT
     ),
     [1] = LAYOUT_tkl_ansi(
         KC_TAB,  KC_Q,    KC_W,      KC_E,     KC_R,       KC_T,      KC_Y,     KC_U,    KC_I,    KC_O,     KC_P,     KC_BSPC,
         QK_GESC, KC_A,    KC_S,      KC_D,     KC_F,       KC_G,      KC_H,     KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_ENT,
         KC_LSFT, KC_Z,    KC_X,      KC_C,     KC_V,       KC_B,      KC_N,     KC_M,    KC_COMM, KC_DOT,   KC_UP ,   KC_QUOT,
-        MO(3),   KC_LCTL, KC_GRV,    KC_LALT,  KC_LGUI,               KC_SPC,   KC_RALT, KC_SLSH, KC_LEFT,  KC_DOWN,  KC_RGHT
+        MO(3),   KC_LCTL, KC_GRV,    KC_LALT,  KC_LGUI,               KC_SPC,   MO(4),   KC_SLSH, KC_LEFT,  KC_DOWN,  KC_RGHT
     ),
     [2] = LAYOUT_tkl_ansi(
         KC_GRV,  KC_1,    KC_2,      KC_3,     KC_4,       KC_5,      KC_6,     KC_7,    KC_8,    KC_9,     KC_0,     KC_EQL,
-        EE_CLR,  MD_BLE1, MD_BLE2,   MD_BLE3,  MD_24G,     MD_USB,    RM_NEXT,  TO(0),   KC_LBRC, KC_RBRC,  KC_BSLS,  KC_MINS,
+        EE_CLR,  MD_BLE1, MD_BLE2,   MD_BLE3,  MD_24G,     MD_USB,    RM_NEXT,  TO(1),   KC_LBRC, KC_RBRC,  KC_BSLS,  KC_MINS,
         KC_LSFT, KC_INS,  KC_DEL,    KC_HOME,  KC_END,     KC_PGUP,   KC_PGDN,  KC_M,    KC_COMM, KC_DOT,   RM_VALU , RM_TOGG,
-        KC_NO,   KC_DEL,  KC_LCTL,   KC_LGUI,  KC_LALT,               QK_BAT,   KC_RALT, KC_EQL,  RM_SPDD,  RM_VALD,  RM_SPDU
+        KC_NO,   KC_DEL,  KC_LCTL,   KC_LGUI,  KC_LALT,               QK_BAT,   MO(4),   KC_EQL,  RM_SPDD,  RM_VALD,  RM_SPDU
     ),
     [3] = LAYOUT_tkl_ansi(
         KC_GRV,  KC_1,    KC_2,      KC_3,     KC_4,       KC_5,      KC_6,     KC_7,    KC_8,    KC_9,     KC_0,     KC_EQL,
         EE_CLR,  MD_BLE1, MD_BLE2,   MD_BLE3,  MD_24G,     MD_USB,    RM_NEXT,  TO(0),   KC_LBRC, KC_RBRC,  KC_BSLS,  KC_MINS,
         KC_LSFT, KC_INS,  KC_DEL,    KC_HOME,  KC_END,     KC_PGUP,   KC_PGDN,  KC_M,    KC_COMM, KC_DOT,   RM_VALU , RM_TOGG,
-        KC_NO,   KC_DEL,  KC_LCTL,   KC_LALT,  KC_LGUI,               QK_BAT,   KC_RALT, KC_EQL,  RM_SPDD,  RM_VALD,  RM_SPDU
-    )
+        KC_NO,   KC_DEL,  KC_LCTL,   KC_LALT,  KC_LGUI,               QK_BAT,   MO(4),   KC_EQL,  RM_SPDD,  RM_VALD,  RM_SPDU
+    ),
+    [4] = LAYOUT_tkl_ansi(
+        KC_TILD, KC_F1,   KC_F2,     KC_F3,    KC_F4,      KC_F5,     KC_F6,    KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_NUM,
+        _______, _______, _______,   _______,  _______,    _______,   RM_NEXT,  _______, KC_LBRC, KC_RBRC,  KC_BSLS,  KC_PSCR,
+        KC_LSFT, KC_INS,  KC_DEL,    KC_HOME,  KC_END,     KC_PGUP,   KC_PGDN,  _______, KC_COMM, _______,  _______,  _______,
+        _______, _______, _______,   _______,  _______,               _______,  _______, _______, _______,  _______,  _______
+    ),
+
 };
 // clang-format on
